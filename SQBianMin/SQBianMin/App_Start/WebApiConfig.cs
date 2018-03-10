308ddc7fbf4bbbac5c73ccee8bcef54bd49a1c9f
zp <651649026@qq.com>
2018-03-10 19:10:35 +0800

    样式修改

---
+++ SQBianMin/SQBianMin/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace SQBianMin
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            //2.自定义路由一：匹配到action
+            config.Routes.MapHttpRoute(
+                name: "ActionApi",
+                routeTemplate: "api/{controller}/{action}/id",
+                defaults: new { id = 1 }
+            );
+        }
+    }
+}
--- SQBianMin/SQBianMin/Models/Enum.cs
+++ SQBianMin/SQBianMin/Models/Enum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,13 @@ namespace SQBianMin.Models
     {
         public enum Category
         {
+            [Description("其他")]
             其他 = 0,
+            [Description("招聘")]
             招聘 = 1,
+            [Description("二手交易")]
             二手交易 = 2,
+            [Description("兼职")]
             兼职 = 3,
 
         }
